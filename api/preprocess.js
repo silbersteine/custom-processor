4e721630f34a6c2742153210a09c2c825efb7e7f
Eric Silberstein <ericsilberstein1@gmail.com>
2023-08-15 13:08:07 +0000

    Update preprocess.js

--- api/preprocess.js
+++ api/preprocess.js
@@ -1,4 +1,22 @@
-export default async function handler(request, response) {    
+function unescapeXmlEntities(str) {
+  return str.replace(/&([a-zA-Z0-9]+);/g, function(match, entity) {
+    switch (entity) {
+      case '&gt;':
+        return '>';
+      case '&lt;':
+        return '<';
+      case '&amp;':
+        return '&';
+      // Add more cases for other escaped entities if needed
+      default:
+        return match; // Return the original match if the entity is not recognized
+    }
+  });
+}
+
+  export default async function handler(request, response) { 
+  
+  
     // Get the payload from the request:
     const payload = request.body
     // Iterate over the keys in the collection:
@@ -6,23 +24,7 @@ export default async function handler(request, response) {
       // Iterate over the translations in the key:
       for (const [lang, v] of Object.entries(keyValue.translations)) {
         
-        // Process the value of the translation:
-        function unescapeXmlEntities(str) {
-            return str.replace(/&([a-zA-Z0-9]+);/g, function(match, entity) {
-              switch (entity) {
-                case '&gt;':
-                  return '>';
-                case '&lt;':
-                  return '<';
-                case '&amp;':
-                  return '&';
-                // Add more cases for other escaped entities if needed
-                default:
-                  return match; // Return the original match if the entity is not recognized
-              }
-            });
-          }
-          
+        // Process the value of the translation:         
           payload.collection.keys[keyId].translations[lang].translation = unescapeXmlEntities(v.translation);
       }
     }
