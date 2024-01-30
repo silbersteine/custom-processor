52d8cba2ab862bf3ba50c02916b1872faefaffc7
silbersteine <79596324+silbersteine@users.noreply.github.com>
2024-01-30 10:39:46 -0400

    Update postprocess.js

--- api/postprocess.js
+++ api/postprocess.js
@@ -5,8 +5,9 @@ async function handler(request, response) {
     // Iterate over the translations in the key:
     for (const [lang, v] of Object.entries(keyValue.translations)) {
       payload.collection.keys[keyId].translations[lang].translation = payload.collection.keys[keyId].translations[lang].translation
-        //.replace(/</g, '&lt;')
-        .replace(/li/g, '$1%@$2')
+        //.replace(/</g, '&lt;');
+        .replace(/li/g, '$1%@$2')l;
+        .replace(/perro/g, 'hello world');
     }
   }
   response.send(payload);
