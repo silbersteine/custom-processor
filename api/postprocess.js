80b5bf45b4930348549ab902a141be4af00f1042
silbersteine <79596324+silbersteine@users.noreply.github.com>
2024-01-30 10:33:53 -0400

    Update postprocess.js

--- api/postprocess.js
+++ api/postprocess.js
@@ -1,13 +1,17 @@
-export default async function handler(request, response) {
-    const payload = request.body
-    // Iterate over the keys in the collection:
-    for (const [keyId, keyValue] of Object.entries(payload.collection.keys)) {
-      // Iterate over the translations in the key:
-      for (const [lang, v] of Object.entries(keyValue.translations)) {
-         payload.collection.keys[keyId].translations[lang].translation = payload.collection.keys[keyId].translations[lang].translation
-         .replace(/</g, '&lt;');
-         .replace(/(<string>)li(<\/string>)/g, '$1%@$2');
-      }
+async function handler(request, response) {
+  const payload = request.body;
+  // Iterate over the keys in the collection:
+  for (const [keyId, keyValue] of Object.entries(payload.collection.keys)) {
+    // Iterate over the translations in the key:
+    for (const [lang, v] of Object.entries(keyValue.translations)) {
+      payload.collection.keys[keyId].translations[lang].translation = payload.collection.keys[keyId].translations[lang].translation
+        .replace(/</g, '&lt;')
+        .replace(/(<string>)li(<\/string>)/g, '$1%@$2')
     }
-    response.send(payload);
+  }
+  response.send(payload);
 }
+
+module.exports = {
+  handler
+};
